e4f54287d29f389cb30e97afa37212e3c32aa165
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-15 10:58:00 -0500

    Exposed tabBar buttons

--- src/components/featuresSection/components/tabBar/tabBar.js
+++ src/components/featuresSection/components/tabBar/tabBar.js
@@ -3,22 +3,21 @@ const tabText = ['control suite', 'tracker', 'showcase', 'monetizer'];
 const tabBar = ((tabText)=>{
     const node = bootstrap.createComponent('container',['d-flex','justify-content-center']).node;
 
-    const init = ((mainNode)=>{
+    const buttons = ((tabText)=>{
+        const buttonClasses = ['btn-outline-primary','btn-outline-success','btn-outline-danger','btn-outline-warning'];
+        const buttons = [];
+        tabText.forEach((header,index)=>{
+            let button = document.createElement('button');
+            let cssClasses = ['btn'];
+            cssClasses.push(buttonClasses[index % buttonClasses.length]);
+            cssClasses.forEach((cssClass)=>{button.classList.toggle(cssClass)});
+            button.textContent = header.toUpperCase();
+            buttons.push(button);
+        });
+        return buttons;
+    })(tabText);
 
-        const tabButtons = ((tabText)=>{
-            const buttonClasses = ['btn-outline-primary','btn-outline-success','btn-outline-danger','btn-outline-warning'];
-            const buttons = [];
-            tabText.forEach((header,index)=>{
-                let button = document.createElement('button');
-                let cssClasses = ['btn'];
-                cssClasses.push(buttonClasses[index % buttonClasses.length]);
-                cssClasses.forEach((cssClass)=>{button.classList.toggle(cssClass)});
-                button.textContent = header.toUpperCase();
-                buttons.push(button);
-            });
-            return buttons;
-        })(tabText);
-
+    const init = ((mainNode, tabButtons)=>{
         const buttonGroup = ((tabButtons)=>{
             const container = bootstrap.createComponent('btn-group');
             tabButtons.forEach((tabButton)=>{container.node.appendChild(tabButton)});
@@ -30,8 +29,8 @@ const tabBar = ((tabText)=>{
             children.forEach((child)=>{mainNode.appendChild(child)})
         })(mainNode,[buttonGroup]);
 
-    })(node);
-    return {node};
+    })(node,buttons);
+    return {node,buttons};
 })(tabText);
 
 module.exports = {
