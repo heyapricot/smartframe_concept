36f78fe08715c95c2a91521ed0b13c8ce0ae40b6
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-16 11:53:57 -0500

    cardDeck can split the cards in rows and columns. It adds empty cards to keep the column structure

--- src/components/featuresSection/components/cardDeck/cardDeck.js
+++ src/components/featuresSection/components/cardDeck/cardDeck.js
@@ -1,18 +1,34 @@
 const {bootstrapComponents:bootstrap} = require('../../../bootstrapComponents/bootstrapComponents');
 const cardDeck = (cardTitles, columnQuantity) => {
-    const node = bootstrap.createComponent('card-deck').node;
-    const rows = ((cardTitles)=>{
-        const rowQuantity = Math.ceil(cardTitles.length / columnQuantity);
-    })(cardTitles, columnQuantity);
-    let cards = ((cardTitles, parentNode)=>{
+    const node = bootstrap.createComponent('container').node;
+    const rowQuantity = Math.ceil(cardTitles.length / columnQuantity);
+    const rows = ((cardTitles, rowQuantity, parentNode)=>{
+        const rowArray = [];
+        for (let i = 0; i < rowQuantity; i++){
+            let row = bootstrap.Row(1,[],parentNode);
+            let deck = bootstrap.createComponent('card-deck',[],row.columns[0].node);
+            rowArray.push(deck);
+        }
+        return rowArray;
+    })(cardTitles, rowQuantity, node);
+
+    let cards = ((cardTitles, cardQuantity)=>{
         let cardArray = [];
-        cardTitles.forEach((title)=>{
-            let card = bootstrap.Card(title);
+        let contentLength = cardTitles.length;
+        for(let i = 0; i < cardQuantity; i++){
+            let card = NaN;
+            i < contentLength ? card = bootstrap.Card(cardTitles[i]) : card = bootstrap.Card('');
             cardArray.push(card);
-            parentNode.appendChild(card.node);
-        });
+        }
         return cardArray;
-    })(cardTitles, node);
+    })(cardTitles, rowQuantity * columnQuantity);
+
+    const insertCards = ((cards,rows,columnQuantity)=>{
+        cards.forEach((card,index)=>{
+            let currentRow = Math.floor(index / columnQuantity);
+            rows[currentRow].node.appendChild(card.node);
+        });
+    })(cards,rows,columnQuantity);
 
     return {cards, node}
 };
