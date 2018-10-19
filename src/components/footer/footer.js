07a0a6ff93415dff734559fc9a4df42c67010063
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-19 17:39:55 -0500

    Footer can create columns from object. Headers are blue and centered

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,30 +1,50 @@
 const {bootstrapComponents:bootstrap} = require('../bootstrapComponents/bootstrapComponents');
-const footer = (()=>{
+
+let footerInfo = {"columns":[
+        {"header":"contact",
+         "links":[
+             {"text":["info@smartframe.io"],"href":"mailto:info@smartframe.io"},
+             {"text":["+44 (0) 208 123 8200"],"href":"#"},
+             {"text":["Pixelrights Limited", "27-31 Clerkenwell Close", "London", "United Kingdom", "ECIR 0AT"],"href":"#"},
+         ]
+        },
+        {"header":"products",
+         "links":[
+             {"text":["SF Cloud"], "href":"#"},
+             {"text":["SF Tracker"], "href":"#"},
+             {"text":["SF Editor"], "href":"#"},
+             {"text":["SF Monetizer"], "href":"#"},
+         ]
+        },
+    ]};
+
+const footer = ((info)=>{
     let node = document.createElement('footer');
 
     let container = ((cssClasses, parentNode)=>{
         let node = document.createElement('div');
         cssClasses.forEach((cssClass)=>{node.classList.toggle(cssClass)});
         parentNode.appendChild(node);
-        let row = bootstrap.Row(5,[],node);
-        let columns = ((columns, headers)=>{
+        let row = bootstrap.Row(info.columns.length,[],node);
+        let columns = ((columns, cssClasses)=>{
             columns.map((column, index)=>{
+                cssClasses.forEach((cssclass)=>{ column.node.classList.toggle(cssclass) });
                 let header = ((headerText, cssClasses, parentNode)=>{
                     let node = document.createElement('h6');
                     node.textContent = headerText.toUpperCase();
                     cssClasses.forEach((cssClass)=>{ node.classList.toggle(cssClass) });
                     parentNode.appendChild(node);
-                })(headers[index], ['text-primary'], column.node);
+                })(info.columns[index].header, ['text-primary'], column.node);
                 return {header}
             });
-        })(row.columns, ['contact', 'products', 'technology', 'support', 'company']);
+        })(row.columns,['text-center']);
 
         return {columns,node,row}
 
     })(['container'], node);
 
     return {container, node}
-})();
+})(footerInfo);
 
 module.exports = {
     footer:footer
